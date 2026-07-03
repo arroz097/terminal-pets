57572f40ad9ef1e88f422e5bb00c31c544e04d60
arroz097 <luizgabrielmatera@hotmail.com>
2026-07-02 23:49:55 -0300

    update demo for display test

--- main.lua
+++ main.lua
@@ -1,17 +1,61 @@
 --local socket = require("socket")
 --local ansi = require("lib.ansi")
 
+math.randomseed(os.time())
+
 local util = require("lib.util")
 
 local cat = require("instances.cat")
 local dog = require("instances.dog")
 local fox = require("instances.fox")
 
-local fabricio = cat.new("fabricio")
+local fabricio = cat.new("fabricioooooooooooo")
 local jorge = dog.new("jorge")
 local raposo = fox.new("raposo")
 
 jorge:showItems()
+
+jorge:getStats()
+
+jorge:dig()
+
+jorge:sleep()
+jorge:eat()
+
+for _ = 1, 4 do
+	jorge:bark()
+	util.sleep(0.5)
+end
+
+jorge.energy = 10
+jorge:dig()
+jorge:dig()
+jorge.energy = 10
+jorge:dig()
+jorge:dig()
+jorge.energy = 10
+jorge:dig()
+jorge:dig()
+
+jorge:showItems()
+jorge:getLogs()
+
+fabricio.energy = 6
+fabricio:nap()
+
+fabricio:getStats()
+
+jorge:bark()
+jorge:bark()
+
+jorge:getStats()
+
+fabricio:drainHunger()
+fabricio:meow()
+
+fabricio:getStats()
+
+--[[
 
 fabricio:sleep()
 fabricio:meow()
@@ -47,3 +91,5 @@ jorge:showItems()
 jorge:howl()
 
 jorge:getLogs()
+
+]]
