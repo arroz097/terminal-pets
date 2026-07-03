a2d53fd9b85b2c7515e9a586282c1a4562839088
arroz097 <luizgabrielmatera@hotmail.com>
2026-07-02 23:15:22 -0300

    improve general display and add state observer messages

--- instances/animal.lua
+++ instances/animal.lua
@@ -13,6 +13,20 @@ local signal = require("lib.signal")
 local animal = {}
 animal.__index = animal
 
+local hungryMessages = {
+	"seems hungry..",
+	"is starving!",
+	"could use some food..",
+	"stomach growls..",
+}
+
+local energyMessages = {
+	"seems tired..",
+	"is exhausted..",
+	"needs some rest..",
+	"is running low on energy..",
+}
+
 ---@param name string
 ---@return animal
 function animal.new(name)
@@ -29,6 +43,17 @@ function animal.new(name)
 
 	self.changed:Connect(function(action)
 		table.insert(self.logs, action)
+
+		local shouldAct = math.random(2) -- 50% chance
+
+		if shouldAct ~= 1 then return end
+
+		if self.energy < 3 then
+			print(string.format("%s %s%s%s", self.name, ansi.text.italic, energyMessages[math.random(#energyMessages)], ansi.text.reset))
+		elseif self.hunger < 3 then
+			print(string.format("%s %s%s%s", self.name, ansi.text.italic, hungryMessages[math.random(#hungryMessages)], ansi.text.reset))
+		end
+
 	end)
 
 	return self
@@ -41,9 +66,10 @@ function animal:eat()
 		print(string.format("%s doesn't have enough %senergy%s to eat!", self.name, ansi.color.cyan, ansi.text.reset))
 		return
 	end
+
 	self.energy = math.max(0, self.energy - 1)
 	self.hunger = math.min(10, self.hunger + 1)
-	print(string.format("%s ate food!", self.name))
+	print(string.format("%s ate food! (%s+1 hunger%s)", self.name, ansi.color.yellow, ansi.text.reset))
 
 	self.changed:Fire(string.format("[%s]: ate food", os.date("%H:%M:%S")))
 end
@@ -55,12 +81,14 @@ function animal:sleep()
 		print(string.format("%s is already on max %senergy%s!", self.name, ansi.color.cyan, ansi.text.reset))
 		return
 	end
-	self.energy = math.min(10, self.energy + 1)
-	print(string.format("%s slept a little!", self.name))
 
-	self.changed:Fire(string.format("[%s]: did some sleep", os.date("%H:%M:%s")))
+	self.energy = math.min(10, self.energy + 1)
+	print(string.format("%s slept a little! (%s+1 energy%s)", self.name, ansi.color.cyan, ansi.text.reset))
+
+	self.changed:Fire(string.format("[%s]: did some sleep", os.date("%H:%M:%S")))
 end
 
+--tbd
 function animal:move()
 
 end
@@ -68,15 +96,39 @@ end
 -- return current animal stats.
 function animal:getStats()
 
-	-- talvez um sistema de pegar a maior string e contar caracteres e formar com =
-	print("\n==================")
-	print(string.format("| %s%s%s stats: %s", ansi.text.underline, ansi.text.bold, self.name, ansi.text.reset))
-	print(string.format("| name..: %s", self.name))
-	print(string.format("| type..: %s", self.type))
-	print(string.format("| health: %d", self.health))
-	print(string.format("| energy: %d", self.energy))
-	print(string.format("| hunger: %d", self.hunger))
-	print("==================\n")
+	local title = self.name .. " stats"
+	local bigString = #title
+
+	local energyColor = self.energy > 6 and ansi.color.brightGreen or self.energy > 3 and ansi.color.yellow or ansi.color.red
+	local hungerColor = self.hunger > 6 and ansi.color.brightGreen or self.hunger > 3 and ansi.color.yellow or ansi.color.red
+	local healthColor = self.health > 60 and ansi.color.brightGreen or self.health > 30 and ansi.color.yellow or ansi.color.red
+
+	local lines = {
+		{ text = "name..: " .. self.name, name = "name..: ", value = self.name,	color = nil },
+		{ text = "type..: " .. self.type, name = "type..: ", value = self.type,	color = nil },
+		{ text = "health: " .. self.health, name = "health: ", value = self.health, color = healthColor },
+		{ text = "energy: " .. self.energy, name = "energy: ", value = self.energy, color = energyColor },
+		{ text = "hunger: " .. self.hunger, name = "hunger: ", value = self.hunger, color = hungerColor },
+	}
+
+	for _, line in ipairs(lines) do
+		if #line.text > bigString then
+			bigString = #line.text
+		end
+	end
+
+	bigString = math.max(bigString, 15) + 4
+
+	io.write(string.format("\n%s\n", string.rep("=", bigString)))
+	print(string.format("| %s%s%s %s|", ansi.text.bold, title, ansi.text.reset, string.rep(" ", (bigString - #title) - 4)))
+	print(string.format("%s", string.rep("=", bigString)))
+
+	for _, line in ipairs(lines) do
+		local color = line.color or "" -- "" representa string vazia nula sem caracteres
+		print(string.format("| %s%s%s%s %s|", line.name, color, line.value, ansi.text.reset, string.rep(" ", (bigString - #line.text) - 4)))
+	end
+
+	print(string.format("%s\n", string.rep("=", bigString)))
 end
 
 function animal:getLogs()
@@ -85,7 +137,7 @@ function animal:getLogs()
 		return
 	end
 
-	local title = self.name .. " log history:"
+	local title = self.name .. " log history"
 	local bigString = #title
 
 	for _, log in ipairs(self.logs) do
@@ -94,15 +146,11 @@ function animal:getLogs()
 		end
 	end
 
-	bigString = math.max(bigString, 30)
-
-	if bigString > 30 then
-		bigString = bigString + 4
-	end
-
+	bigString = math.max(bigString, 30) + 4
 
 	io.write(string.format("\n%s\n", string.rep("=", bigString)))
-	print(string.format("| %s%s%s%s %s|", ansi.text.underline, ansi.text.bold, title, ansi.text.reset, string.rep(" ", (bigString - #title) - 4)))
+	print(string.format("| %s%s%s %s|", ansi.text.bold, title, ansi.text.reset, string.rep(" ", (bigString - #title) - 4)))
+	print(string.format("%s", string.rep("=", bigString)))
 
 	for _, log in ipairs(self.logs) do
 		print(string.format("| %s %s|", log, string.rep(" ", (bigString - #log) - 4)))
@@ -116,7 +164,7 @@ function animal:drainHunger()
 	while self.hunger > 0 do
 		util.sleep(1)
 		self.hunger = math.max(0, self.hunger - 1)
-		print(string.format("%s %shunger%s now is %d", self.name, ansi.color.brightYellow, ansi.text.reset, self.hunger))
+		print(string.format("%s %shunger%s now is %d", self.name, ansi.color.yellow, ansi.text.reset, self.hunger))
 	end
 	self.changed:Fire(string.format("[%s]: drained hunger", os.date("%H:%M:%S")))
 end
