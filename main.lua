8375f52f776983b3920e9e0d121643f48001b795
arroz097 <luizgabrielmatera@hotmail.com>
2026-07-05 00:35:39 -0300

    animal creation flow and command dispatch loop

--- main.lua
+++ main.lua
@@ -1,95 +1,61 @@
---local socket = require("socket")
---local ansi = require("lib.ansi")
-
 math.randomseed(os.time())
 
+--local socket = require("socket")
+local ansi = require("lib.ansi")
 local util = require("lib.util")
 
-local cat = require("instances.cat")
-local dog = require("instances.dog")
-local fox = require("instances.fox")
+local animals = {
+	cat = require("instances.cat"),
+	dog = require("instances.dog"),
+	fox = require("instances.fox"),
+}
 
-local fabricio = cat.new("fabricioooooooooooo")
-local jorge = dog.new("jorge")
-local raposo = fox.new("raposo")
+ansi:enterScreen()
 
-jorge:showItems()
+print("animal types...: cat | dog | fox\n")
+print("animal commands: eat | sleep | getStats | showInventory | getLogs | drainHunger")
+print("dog commands...: bark | dig | howl")
+print("cat commands...: meow | scratch | nap | purr | hiss\n")
+print("exit to leave\n")
 
-jorge:getStats()
+io.write("your animal type: ")
+io.flush()
+local animalType = io.read()
 
-jorge:dig()
-
-jorge:sleep()
-jorge:eat()
-
-for _ = 1, 4 do
-	jorge:bark()
-	util.sleep(0.5)
+if not animals[animalType] then -- mudar pra validação com repeat until depois
+	io.write(string.format("\n%s is not a valid animal\n", animalType))
+	util.sleep(1)
+	ansi:exitScreen()
+	return
 end
 
-jorge.energy = 10
-jorge:dig()
-jorge:dig()
-jorge.energy = 10
-jorge:dig()
-jorge:dig()
-jorge.energy = 10
-jorge:dig()
-jorge:dig()
+local name
+repeat
+	io.write(string.format("%s name: ", animalType))
+	io.flush()
+	name = io.read()
 
-jorge:showItems()
-jorge:getLogs()
+until name ~= ""
 
-fabricio.energy = 6
-fabricio:nap()
+local pet = animals[animalType].new(name)
+print()
 
-fabricio:getStats()
+local command
+repeat
+	command = io.read()
 
-jorge:bark()
-jorge:bark()
+	if command == "exit" then
+		--
+	elseif pet[command] then
+		pet[command](pet)
+	else
+		io.write(string.format("\n\"%s\" is not a valid method of animal %s\n", command, name))
+		io.flush()
+	end
 
-jorge:getStats()
+until command == "exit"
 
-fabricio:drainHunger()
-fabricio:meow()
-
-fabricio:getStats()
-
---[[
-
-fabricio:sleep()
-fabricio:meow()
-
-fabricio:eat()
-
-jorge:bark()
-jorge:dig()
-jorge:dig()
-jorge:dig()
-
-jorge.energy = 10
-
-jorge:dig()
-
-for _ = 1, 10 do
-	fabricio:meow()
-	util.sleep(0.5)
-end
-
-fabricio:nap()
-fabricio:getStats()
-
-jorge:dig()
-
-fabricio:getLogs()
-
-jorge:dig()
-jorge:fetch("tilapia")
-jorge:getStats()
-jorge:bark()
-jorge:showItems()
-jorge:howl()
-
-jorge:getLogs()
-
-]]
+io.write("\nleaving...")
+io.flush()
+util.sleep(1)
+ansi:exitScreen()
