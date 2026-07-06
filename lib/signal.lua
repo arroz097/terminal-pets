08b09cf34d41f9b739c1eadf545ce8a1bbf38e71
arroz097 <luizgabrielmatera@hotmail.com>
2026-07-06 15:56:43 -0300

    separate connection into its own class

--- lib/signal.lua
+++ lib/signal.lua
@@ -8,7 +8,25 @@ signal.__index = signal
 ---@class connection
 ---@field _fn function
 ---@field _signal table
----@field Disconnect fun(self: connection)
+local connection = {}
+connection.__index = connection
+
+---@return connection
+function connection.new(fn, sig)
+	return setmetatable({ _fn = fn, _signal = sig }, connection)
+end
+
+function connection:Disconnect()
+	for i, fn in ipairs(self._signal._listeners) do
+		if fn == self._fn then
+			table.remove(self._signal._listeners, i)
+			break
+		end
+	end
+
+	self._fn = nil
+	self._signal = nil
+end
 
 ---@return signal
 function signal.new()
@@ -18,25 +36,10 @@ end
 ---@param callback function
 ---@return connection
 function signal:Connect(callback)
-	local connection = { _fn = callback, _signal = self }
-	-- _fn guarda o callback antigo para futuro Disconnect
-	-- _signal guarda o self principal
-
-	function connection:Disconnect()
-		-- self aqui referencia o connection
-		for i, fn in ipairs(self._signal._listeners) do
-			if fn == self._fn then
-				table.remove(self._signal._listeners, i)
-				break
-			end
-		end
-
-		self._fn = nil
-		self._signal = nil
-	end
+	local conn = connection.new(callback, self)
 
 	table.insert(self._listeners, callback)
-	return connection
+	return conn
 end
 
 ---@param ... any
