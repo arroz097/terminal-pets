0610db355f14d501998d8e42c42711bfe2cf3d1c
arroz097 <luizgabrielmatera@hotmail.com>
2026-07-06 01:48:03 -0300

    fix loop and refine messages

--- main.lua
+++ main.lua
@@ -12,50 +12,50 @@ local animals = {
 
 ansi:enterScreen()
 
-print("animal types...: cat | dog | fox\n")
-print("animal commands: eat | sleep | getStats | showInventory | getLogs | drainHunger")
-print("dog commands...: bark | dig | howl")
-print("cat commands...: meow | scratch | nap | purr | hiss\n")
-print("exit to leave\n")
+print("animal types...: cat, dog, fox\n")
+print("animal commands: eat, sleep, getStats, getLogs, showInventory, drainHunger\n")
+print(string.format("%sgetMethods%s to list current animal actions. \n", ansi.text.italic, ansi.text.reset))
 
-io.write("your animal type: ")
-io.flush()
-local animalType = io.read()
+local animalType
+repeat
+	io.write(ansi.text.bold, "animal type: ", ansi.text.reset)
+	io.flush()
+	animalType = io.read()
 
-if not animals[animalType] then -- mudar pra validação com repeat until depois
-	io.write(string.format("\n%s is not a valid animal\n", animalType))
-	util.sleep(1)
-	ansi:exitScreen()
-	return
-end
+until animals[animalType]
 
 local name
 repeat
-	io.write(string.format("%s name: ", animalType))
+	io.write(ansi.text.bold, "animal name: ", ansi.text.reset)
 	io.flush()
 	name = io.read()
 
 until name ~= ""
 
+--io.write(ansi:moveTo(2, 1), ansi.clearDown)
 local pet = animals[animalType].new(name)
 print()
+print(string.format("%sexit%s to leave.\n", ansi.text.italic, ansi.text.reset))
 
 local command
 repeat
+	io.write(string.format("%s action: ", name))
+	io.flush()
 	command = io.read()
 
 	if command == "exit" then
 		--
-	elseif pet[command] then
+	elseif command ~= "new" and command ~= "__index" and pet[command] then
 		pet[command](pet)
 	else
-		io.write(string.format("\n\"%s\" is not a valid method of animal %s\n", command, name))
+		io.write(string.format("\n\"%s\" is not a valid method of %s %s\n", command, animalType, name))
 		io.flush()
 	end
 
 until command == "exit"
 
-io.write("\nleaving...")
+io.write(string.format("\nleaving %s...", name))
 io.flush()
 util.sleep(1)
+
 ansi:exitScreen()
